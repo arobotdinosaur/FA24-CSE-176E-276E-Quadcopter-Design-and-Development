687c2747dd0d802b6e4a512c411beb8cb6796811
arobotdinosaur <160431417+arobotdinosaur@users.noreply.github.com>
2024-10-08 11:59:51 -0700

    add quad and remote firmware

---
+++ firmware/remote_firmware/remote_firmware.ino
@@ -0,0 +1,14 @@
+#include <radio.h>
+#include "quad_remote.h"      // Header file with pin definitions and setup
+#include <serLCD.h>
+serLCD lcd;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
