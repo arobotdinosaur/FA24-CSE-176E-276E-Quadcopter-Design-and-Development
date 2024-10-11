e3d82e90ef152e9fafb8175bcf008f29b9014a98
Kaustubh <kkanagalekar@ucsd.edu>
2024-10-11 15:49:55 -0700

    edited quad_firmware.ino

--- firmware/quad_firmware/quad_firmware.ino
+++ firmware/quad_firmware/quad_firmware.ino
@@ -1,6 +1,16 @@
 void setup() {
+  //rear is where the 6 pins are sticking out
+  const int left_rear = 8;
+  const int right_rear = 3;
+  const int left_top = 5; 
+  const int right_top = 4; 
+
 }
 
 void loop() {
+  analogWrite(8, 10);
+  analogWrite(3, 10);
+  analogWrite(5, 10);
+  analogWrite(4, 10);
 }
 
