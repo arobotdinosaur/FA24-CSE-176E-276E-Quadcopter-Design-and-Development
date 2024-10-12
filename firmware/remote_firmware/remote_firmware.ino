c13cccf1d551a2c755bc01e10d8582b858397ecd
Kaustubh <kkanagalekar@ucsd.edu>
2024-10-11 22:43:49 -0700

    partial code for calibration

--- firmware/remote_firmware/remote_firmware.ino
+++ firmware/remote_firmware/remote_firmware.ino
@@ -24,38 +24,73 @@ struct flightControl{
 
   //const int magicnumber = 5700;
 };
+
+//data corresponding to max and min gimbal (top-bottom)
+int gimbal_value_l = 0;
+int gimbal_value_r = 0;
+int max_gimbal = 1023; 
+int min_gimbal = 0; 
+
 //battery voltage seems to be capped at 57 using analogReference(INTERNAL);
 void setup() {
-  // put your setup code here, to run once:
+  
 	const int SERIAL_BAUD = 9600 ;        // Baud rate for serial port 
 	Serial.begin(SERIAL_BAUD);          // Start up serial
 	delay(1000);
-  const int BAT_SENSE_PIN = A7; 
+  const int BAT_SENSE_PIN = A7;   //battery pin 
   pinMode(BAT_SENSE_PIN, INPUT);
   analogReference(INTERNAL);
+
 	quad_remote_setup();
-  rfBegin(12);
+
+  rfBegin(12); //radio stuff
   rfPrint("ATmega128RFA1 Dev Board Online!\r\n");
+
+  //testing for gimbal calibration 
+  while (millis() < 3010) {
+    gimbal_value_l = analogRead(A1);
+    gimbal_value_r = analogRead(A3);
+
+    if (gimbal_value_l > max_gimbal){
+      int max_gimbal_l = gimbal_value_l; 
+    }
+
+    if (gimbal_value_r > max_gimbal){
+      int max_gimbal_r = gimbal_value_r; 
+    }
+  }
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //This was very heavily inspired by the AnalogReadSerial Example
+  //involves battery and gimbal control
   int leftsideways = analogRead(A0);
   int leftupdown = analogRead(A1);
   int rightsideways = analogRead(A2);
   int rightupdown = analogRead(A3);
   int BAT_SENSE_PIN = analogRead(A7); 
-  leftsideways = constrain(leftsideways, 0,255);//constraining as per instructions
-  leftupdown = constrain(leftupdown, 0, 255);//constraining as per instructions
-  rightsideways = contstrain(rightsideways, 0,255); //constraining as per instructions
-  rightupdown = constrain(rightupdown, 0, 255);//constraining as per instructions
+  //part of calibration 
+  
+  leftsideways = constrain(leftsideways, min_gimbal_l,max_gimbal_l);//constraining as per instructions
+  leftupdown = constrain(leftupdown, min_gimbal_l, max_gimbal_l);//constraining as per instructions
+  rightsideways = contstrain(rightsideways, min_gimbal_r,max_gimbal_r); //constraining as per instructions
+  rightupdown = constrain(rightupdown, min_gimbal_r, max_gimbal_r);//constraining as per instructions
+  leftsideways = map(leftsideways, min_gimbal_l, max_gimbal_l, 0, 255);
+  rightsideways = map(rightsideways, min_gimbal_r, max_gimbal_r, 0, 255);
+  rightupdown = map(rightupdown, min_gimbal_r, max_gimbal_r, 0, 255);
+  leftupdown = map(leftupdown, min_gimbal_l, max_gimbal_l, 0, 255);
  //Serial.println(leftsideways);
   //Serial.println(leftupdown);
   //Serial.println(rightsideways);
   //Serial.println(rightupdown);
   Serial.print("Battery Voltage:"); 
   Serial.println(BAT_SENSE_PIN);
+
+
+//// this is test code for radio communication 
+
   //uint8_t b[256];
   //int len;
   //if (len = rfAvailable())  // If serial comes in...
