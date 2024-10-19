bd7c2b04faafe055c5b26cca1e56191e1d35ddd7
Kaustubh <kkanagalekar@ucsd.edu>
2024-10-18 17:39:58 -0700

    check issue with code for data acq and graph

--- firmware/quad_firmware/quad_firmware.ino
+++ firmware/quad_firmware/quad_firmware.ino
@@ -1,4 +1,8 @@
 #include <radio.h>
+#include <Adafruit_Simple_AHRS.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <QuadClass_LSM6DSOX.h>
 
   const int left_rear = 8;
   const int right_rear = 3;
@@ -13,6 +17,10 @@
   uint8_t a[4] = {0};
 
 
+  float pitch; 
+  float pitch_rate;
+  QuadClass_LSM6DSOX lsm = QuadClass_LSM6DSOX();
+
 void setup() {
   //copied from rfecho
   rfBegin(12);  // Initialize ATmega128RFA1 radio on channel 11 (can be 11-26)
@@ -26,7 +34,7 @@ void setup() {
   rfWrite(b,4);
 
   //rear is where the 6 pins are sticking out
-  const int SERIAL_BAUD = 9600 ;        // Baud rate for serial port 
+  const int SERIAL_BAUD = 115200 ;        // Baud rate for serial port 
 	Serial.begin(SERIAL_BAUD);          // Start up serial
 
   const int BAT_SENSE_PIN = A7; 
@@ -40,6 +48,36 @@ void setup() {
   const int LED4 = 36; 
   const int PRETTY_LEDS = 34;
 
+}
+
+// sensor setup 
+void setupSensor()
+{
+  // Set data rate for G and XL.  Set G low-pass cut off.  (Section 7.12)
+ // lsm.write8(XGTYPE, Adafruit_LSM9DS1::LSM9DS1_REGISTER_CTRL_REG1_G,  ODR_952 | G_BW_G_10 );  //952hz ODR + 63Hz cuttof
+
+  // Enable the XL (Section 7.23)
+  //lsm.write8(XGTYPE, Adafruit_LSM9DS1::LSM9DS1_REGISTER_CTRL_REG5_XL, XL_ENABLE_X | XL_ENABLE_Y | XL_ENABLE_Z);
+
+  // Set low-pass XL filter frequency divider (Section 7.25)
+  //lsm.write8(XGTYPE, Adafruit_LSM9DS1::LSM9DS1_REGISTER_CTRL_REG7_XL, HR_MODE | XL_LP_ODR_RATIO_9);
+  
+
+  // This only sets range of measurable values for each sensor.  Setting these manually (I.e., without using these functions) will cause incorrect output from the library.
+ // lsm.setupAccel(Adafruit_LSM9DS1::LSM9DS1_ACCELRANGE_2G);
+  //lsm.setupMag(Adafruit_LSM9DS1::LSM9DS1_MAGGAIN_4GAUSS);
+ // lsm.setupGyro(Adafruit_LSM9DS1::LSM9DS1_GYROSCALE_245DPS);
+
+ if (!lsm.begin_I2C()) {
+    Serial.println("Failed to find LSM6DSOX chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  _accel = lsm.getAccelerometerSensor();
+  _gyro = lsm.getGyroSensor();
+  ahrs = new Adafruit_Simple_AHRS(_accel, _mag, _gyro);
+#
 }
 
 //full battery is at 885 max 
@@ -68,6 +106,9 @@ uint8_t a[4] = {0};
     else{
       rfFlush();
     }
+
+
+  
 
   }
 
@@ -133,3 +174,4 @@ uint8_t a[4] = {0};
     }
 }
 
+// obtaining data from imu 
